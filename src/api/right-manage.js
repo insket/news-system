97c428aa447795b391fbfc2285f156d10c89f773
insket <2694183181@qq.com>
2021-10-15 10:57:34 +0800

    '权限删除完成'

--- src/api/right-manage.js
+++ src/api/right-manage.js
@@ -6,6 +6,26 @@ import request from "../utils/request";
 export const getRightList = () => {
   return request({
     method: 'get',
-    url: '/rights'
+    url: '/rights?_embed=children'
+  })
+}
+
+/*
+    删除权限列表选项  一级
+*/
+export const deleteSlideMenuList = (id) => {
+  return request({
+    method: 'delete',
+    url: `/rights/${id}`
+  })
+}
+
+/*
+    删除权限列表选项  二级
+*/
+export const deleteChildrenList = (id) => {
+  return request({
+    method: 'delete',
+    url: `/children/${id}`
   })
 }
--- src/views/sandbox/right-manage/RightList.jsx
+++ src/views/sandbox/right-manage/RightList.jsx
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react'
-import { Table } from 'antd'
-import { getRightList } from '../../../api/right-manage'
+import { Table, Tag, Button, Modal } from 'antd'
+import { getRightList, deleteSlideMenuList, deleteChildrenList } from '../../../api/right-manage'
+import {
+  EditOutlined,
+  DeleteOutlined,
+  ExclamationCircleOutlined
+} from '@ant-design/icons';
+
 
 /*
     权限列表
 */
+
+const { confirm } = Modal;
+
 export default function RightList() {
 
   const [dataSource, setDataSource] = useState([])
@@ -17,7 +26,49 @@ export default function RightList() {
   const rightList = async() => {
     const data = await getRightList()
     // console.log(data)
+    // 找出 没有 children的选项 不渲染展开
+    data.forEach((item) => {
+      if (item.children && item.children.length === 0) {
+        item.children = ''
+      }
+    })
     setDataSource(data)
+  }
+
+  // 弹出确认删除框
+  const isConfirm = (item) => {
+    confirm({
+      title: '确认要删除改权限吗?',
+      icon: <ExclamationCircleOutlined />,
+      content: '如果删除的话，您将会失去一些权限',
+      onOk() {
+        deleteMethod(item)
+      },
+      onCancel() {},
+      okText:"确认",
+      cancelText:"取消"
+    });
+  }
+
+  // q确认后删除权限
+  const deleteMethod = (item) => {
+    // console.log(item.id)
+    // 当前页面同步状态 + 后端同步
+    if (item.grade === 1) {
+      // 过滤出与点击iten.id不同的
+      setDataSource(dataSource.filter(data => data.id !== item.id))
+      // 删除选项
+      deleteSlideMenuList(item.id)
+    }else{
+      // 从 dataSource 中找出与点击二级标签相同 rightId 的父级标签
+      let list = dataSource.filter(data => data.id === item.rightId)
+      // 过滤出与点击标签id不同的
+      // console.log(list)
+      list[0].children = list[0].children.filter(data => data.id !== item.id)
+      setDataSource([...dataSource])
+      deleteChildrenList(item.id)
+    }
+
   }
 
   const columns = [
@@ -25,6 +76,9 @@ export default function RightList() {
       title: 'ID',
       dataIndex: 'id',
       key: 'id',
+      render: (id) => {
+        return <b>{id}</b>
+      }
     },
     {
       title: '权限名称',
@@ -35,17 +89,44 @@ export default function RightList() {
       title: '权限路径',
       dataIndex: 'key',
       key: 'key',
+      render: (key) => {
+        return <Tag color="gold">{key}</Tag>
+      }
     },
     {
       title: '操作',
-      dataIndex: 'pagepermisson',
-      key: 'pagepermisson',
+      render: (item) => {
+        return (
+          <div>
+            <Button
+              shape="circle"
+              type="primary"
+              icon={<EditOutlined />}
+              style={{marginLeft:'-20px'}}
+            >
+            </Button>
+            <Button
+              shape="circle"
+              danger
+              icon={<DeleteOutlined /> }
+              onClick={() => {isConfirm(item)}}
+            >
+            </Button>
+          </div>
+        )
+      }
     }
   ]
 
   return (
     <div>
-      <Table dataSource={dataSource} columns={columns} />
+      <Table
+        dataSource={dataSource}
+        columns={columns}
+        pagination={{
+          pageSize: 5
+        }}
+      />
     </div>
   )
 }
